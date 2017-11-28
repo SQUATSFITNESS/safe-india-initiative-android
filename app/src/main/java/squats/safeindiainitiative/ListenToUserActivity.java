3e72fa9884083813698f140f9ba1817e39522fa4
Souvik Basu <souvikbasu@gmail.com>
2017-11-28 21:25:15 +0530

    add empty activity - ListenToUser

---
+++ app/src/main/java/squats/safeindiainitiative/ListenToUserActivity.java
@@ -0,0 +1,13 @@
+package squats.safeindiainitiative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListenToUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_to_user);
+    }
+}
