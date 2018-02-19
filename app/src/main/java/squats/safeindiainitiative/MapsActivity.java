41abcde4cd18a3602bf30aab46436586996b3fa4
Souvik Basu <souvikbasu@gmail.com>
2018-02-19 21:15:01 +0530

    add nivagation support to reach victim

--- app/src/main/java/squats/safeindiainitiative/MapsActivity.java
+++ app/src/main/java/squats/safeindiainitiative/MapsActivity.java
@@ -1,6 +1,7 @@
 package squats.safeindiainitiative;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
@@ -37,7 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private TextView help_message;
-    private Button canHelpButton;
+    private Button canHelpButton, navigateButton;
     private Double lat, lng;
     private Double myLat, myLng;
     private String helpSeekerFcm;
@@ -71,19 +72,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 new MapsActivity.SendPostRequest().execute(helperUrl, helperPosData);
             }
         });
+
+
+        navigateButton = (Button) this.findViewById(R.id.button_navigate);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=12.93207349,77.63141693&mode=w");
+                Intent intent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(intent);
+
+            }
+        });
     }
 
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -181,6 +185,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             help_message.setText("Please reach the marker on map as soon as possible");
             canHelpButton.setVisibility(View.INVISIBLE);
+            navigateButton.setVisibility(View.VISIBLE);
         }
     }
 }
